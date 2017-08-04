a9c73b0b67bee4f59ca83c6ae016736433297cee
Ben Hunsaker <benhunsaker@us.ibm.com>
2017-08-04 15:27:52 -0500

    added more server configurations

--- configs/general.js
+++ configs/general.js
@@ -7,12 +7,24 @@ module.exports = {
     dbUrl: 'mongodb://localhost:27017',
     hostname: process.env.HOSTNAME || 'localhost',
     port: process.env.PORT || 8000,
+    coverage: {
+        dir: root + '/coverage/combined',
+        reporters: ['json', 'html', 'text', 'lcov']
+        // thresholds: {
+        //     global: 90
+        // }
+    },
     paths: {
         js: [
             `${root}/**/*.js`,
             `!${root}/+(.git|coverage|dist|node_modules)/**/*.js`
         ],
+        json: [
+            '**/*.json',
+            '!coverage/**',
+            '!node_modules/**'
+        ],
         sass: `${root}/assets/styles/**/*.scss`,
-        tests: []
+        tests: [`${root}/tests/**/*.js`]
     }
 };
